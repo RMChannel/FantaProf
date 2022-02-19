7186d1e39e804850987052ebe954614820d66ee0
RMChannel <98057423+RMChannel@users.noreply.github.com>
2022-02-19 08:24:00 +0100

    V1.0A
    Questa versione è la base del sito, mancano ancora molte funzioni ma la pagina home e regolamento sono perfettamente funzionanti

---
+++ script.js
@@ -0,0 +1,21 @@
+const p1score=document.getElementById('prof1score');
+const p2score=document.getElementById('prof2score');
+const p3score=document.getElementById('prof3score');
+const p4score=document.getElementById('prof4score');
+const p5score=document.getElementById('prof5score');
+const p6score=document.getElementById('prof6score');
+const p7score=document.getElementById('prof7score');
+const p8score=document.getElementById('prof8score');
+const p9score=document.getElementById('prof9score');
+
+scorearray=[0,0,0,0,0,0,0,0,0];
+
+p1score.innerText=scorearray[0];
+p2score.innerText=scorearray[1];
+p3score.innerText=scorearray[2];
+p4score.innerText=scorearray[3];
+p5score.innerText=scorearray[4];
+p6score.innerText=scorearray[5];
+p7score.innerText=scorearray[6];
+p8score.innerText=scorearray[7];
+p9score.innerText=scorearray[8];
